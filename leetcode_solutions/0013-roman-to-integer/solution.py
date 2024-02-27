44c09a55064c87ffd93b7aaf6be064df5dc6a54d
Pragya Jain <101741697+pragya-jain-io@users.noreply.github.com>
2024-02-27 17:34:44 +0000

    [LeetCode Sync] Runtime - 28 ms (6.31%), Memory - 11.7 MB (100.00%)

--- leetcode_solutions/0013-roman-to-integer/solution.py
+++ leetcode_solutions/0013-roman-to-integer/solution.py
@@ -1,11 +1,21 @@
-class Solution:
-    def romanToInt(self, s: str) :
-        dictionary={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        summ=0
-        for i in range (len(s)):
-            if i<(len(s)-1) and dictionary[s[i]]<dictionary[s[i+1]] :
-                summ+=(-1*dictionary[s[i]])
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        key={"I":1,"V":5,"X":10,"C":100,"L":50,"D":500,"M":1000}
+        num=0
+        i=0
+        while i< len(s):
+
+            if i<(len(s)-1) and key[s[i]]<key[s[i+1]]:
+                num+=(key[s[i+1]]-key[s[i]])
+                i=i+2
+
             else:
-                summ+=dictionary[s[i]]
-                
-        return summ
+                num+=key[s[i]]
+                i+=1
+        return num
+
+        
