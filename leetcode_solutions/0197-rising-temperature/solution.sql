5f5775c529d42487bd1f289b2a5107d85a25b39c
Pragya Jain <101741697+pragya-jain-io@users.noreply.github.com>
2024-09-16 16:37:15 +0000

    [LeetCode Sync] Runtime - 387 ms (98.51%), Memory - 0.0B (100.00%)

--- leetcode_solutions/0197-rising-temperature/solution.sql
+++ leetcode_solutions/0197-rising-temperature/solution.sql
@@ -1,6 +1,4 @@
-SELECT W1.id
-FROM Weather W1
-JOIN Weather W2
-ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
-WHERE W1.temperature > W2.temperature;
-
+select a.id
+from weather a inner join weather b
+on date_add(a.recorddate, interval -1 day) = b.recorddate
+where a.temperature>b.temperature
