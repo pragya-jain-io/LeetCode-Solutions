18b1dad7dce3f037207d4421b36ceca1876a9ed4
Pragya Jain <101741697+pragya-jain-io@users.noreply.github.com>
2025-03-13 02:45:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 25 MB (9.32%)

--- leetcode_solutions/0540-single-element-in-a-sorted-array/solution.py
+++ leetcode_solutions/0540-single-element-in-a-sorted-array/solution.py
@@ -1,9 +1,37 @@
 class Solution:
+
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums)==1:
+        def check_single(nums,mid):
+            if mid > 0:
+                if nums[mid-1]==nums[mid]:
+                    return False
+            if mid < len(nums) - 1:
+                if nums[mid+1]==nums[mid]:
+                    return False
+            return True
+        if len(nums) == 1:
             return nums[0]
-        for i in range(0,len(nums),2):
-            if i==len(nums)-1:
-                return nums[i]
-            if nums[i]!=nums[i+1]:
-                return nums[i]
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low+high) //2
+            # checK single
+            if check_single(nums,mid):
+                return nums[mid]
+            else:
+                if nums[mid+1]==nums[mid]:
+                    pix = mid + 1
+                    if (high - pix) % 2 != 0:
+                        low = pix + 1
+                    else: 
+                        high = mid - 1
+                else:
+                    pix = mid - 1
+                    if (high - mid) % 2 != 0:
+                        low = mid + 1
+                    else: 
+                        high = pix - 1
+
+                
+
+
